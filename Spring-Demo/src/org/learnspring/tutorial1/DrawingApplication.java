6efca4889b30c75778239dd49dc985e3a649df04
ThejashwiniShetty <thejashwini.srk@gmail.com>
2018-05-26 10:48:38 +0530

    Setter injection using ApplicationContext

--- Spring-Demo/src/org/learnspring/tutorial1/DrawingApplication.java
+++ Spring-Demo/src/org/learnspring/tutorial1/DrawingApplication.java
@@ -1,14 +1,14 @@
 package org.learnspring.tutorial1;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 
--- Spring-Demo/src/org/learnspring/tutorial1/Triangle.java
+++ Spring-Demo/src/org/learnspring/tutorial1/Triangle.java
@@ -2,7 +2,24 @@ package org.learnspring.tutorial1;
 
 public class Triangle {
 
+	private String type;
+	private int height;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public void draw() {
-		System.out.println("Triangle is drawn");
+		System.out.println(getType() +" triangle is drawn is of height "+getHeight());
 	}
 }
